8b0c2b8b15cce93b7adf6849f512cb7e2a522a7e
MathisTrochet <mathis.trochet23@gmail.com>
2023-11-05 11:57:50 +0100

    Main

--- Js/phpCall.js
+++ Js/phpCall.js
@@ -12,3 +12,18 @@ function deconnexion() {
 
     xhr.send();
 }
+ 
+function indexDeconnexion() {
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'deconnexion.php', true);
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            // La session a été détruite côté serveur, vous pouvez maintenant effectuer des actions côté client, comme rediriger l'utilisateur vers la page de connexion.
+            window.location.href = 'Authentification/connexionSEC.php';
+        }
+    };
+
+    xhr.send();
+}
